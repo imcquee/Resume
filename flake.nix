aacff36ff2206711166522a886eab6cdd16fb697
Isaac McQueen <imcqueen@truehomesusa.com>
2024-08-03 21:19:52 -0400

    initial commit

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      devShells."${system}".default =
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell { packages = with pkgs; [ typst ]; };
+    };
+}
